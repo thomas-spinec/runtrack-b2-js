1592cc01da7715d09bf2b6bbebede0efdb879f65
thomas-spinec <thomas.spinec@laplateforme.io>
2023-09-06 17:14:16 +0200

    debut job08 du jour01

---
+++ jour-01/job-08/script.js
@@ -0,0 +1,11 @@
+function myLength(array) {
+    let result = 0;
+    while (array[result] !== undefined) {
+        result++;
+    }
+    return result;
+}
+
+function myArraySort(array, sorting) {
+
+}
