027777ff25080111052d9aec235ee69ca1d012a3
thomas-spinec <thomas.spinec@laplateforme.io>
2023-09-06 18:58:35 +0200

    jour01 job08 done

--- jour-01/job-08/script.js
+++ jour-01/job-08/script.js
@@ -7,5 +7,29 @@ function myLength(array) {
 }
 
 function myArraySort(array, sorting) {
-
+    if (sorting === "ASC") {
+        for (let i=0; i < myLength(array); i++) {
+            for (let j = i+1; j < myLength(array); j++) {
+                if (array[i] > array[j]) {
+                    let temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
+    } else if (sorting === "DESC") {
+        for (let i=0; i < myLength(array); i++) {
+            for (let j = i+1; j < myLength(array); j++) {
+                if (array[i] < array[j]) {
+                    let temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
+    }
+    return array;
 }
+
+// console.log(myArraySort([6, 4, 8, 3, 2, 9, 1, 5, 7], "ASC")); // [1, 2, 3, 4, 5, 6, 7, 8, 9]
+// console.log(myArraySort([6, 4, 8, 3, 2, 9, 1, 5, 7], "DESC")); // [9, 8, 7, 6, 5, 4, 3, 2, 1]
