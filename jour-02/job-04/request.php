23ac30f61ed18b78f718d070388547aab146150e
thomas-spinec <thomas.spinec@laplateforme.io>
2023-09-07 18:46:41 +0200

    jour02 job04 done

---
+++ jour-02/job-04/request.php
@@ -0,0 +1,53 @@
+<?php
+function connect_db(){
+    $pdo = new PDO('mysql:host=localhost;dbname=lp_official', 'root', '');
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    return $pdo;
+}
+function my_insert_student(string $email, string $fullname, string $genre, int $grade_id, Datetime $birthdate) : bool {
+    $pdo = connect_db();
+    $sql = "INSERT INTO student (email, fullname, gender, grade_id, birthdate) VALUES (:email, :fullname, :genre, :grade_id, :birthdate)";
+    $stmt = $pdo->prepare($sql);
+
+    // si la requête s'est bien passé on retourne true
+    if ($stmt->execute([
+        ':email' => $email,
+        ':fullname' => $fullname,
+        ':genre' => $genre,
+        ':grade_id' => $grade_id,
+        ':birthdate' => $birthdate->format('Y-m-d')
+    ])) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function getGrades() : array {
+    $pdo = connect_db();
+    $sql = "SELECT * FROM grade";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute();
+    $grades = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    return $grades;
+}
+
+if(isset($_POST['student-mail'])) {
+    $email = $_POST['student-mail'];
+    $fullname = $_POST['student-fullname'];
+    $genre = $_POST['student-gender'];
+    $grade_id = $_POST['student-grade'];
+    $birthdate = new DateTime($_POST['student-birthdate']);
+    $result = my_insert_student($email, $fullname, $genre, $grade_id, $birthdate);
+    if($result) {
+        echo "Student registered successfully";
+    } else {
+        echo "Error while registering student";
+    }
+}
+
+// s'il y a une requête en GET
+if(isset($_GET['get'])) {
+    $grades = getGrades();
+    echo json_encode($grades);
+}
---
+++ jour-02/job-04/script.js
@@ -0,0 +1,36 @@
+const form = document.querySelector('#form-add-student');
+let select = document.querySelector('#student-grade');
+
+async function getGrade() {
+    let response = await fetch('request.php?get=grade');
+    let result = await response.json();
+    for (let i = 0; i < result.length; i++) {
+        let option = document.createElement('option');
+        option.textContent = result[i].name;
+        option.value = result[i].id;
+        select.appendChild(option);
+    }
+}
+async function myRegisterStudent(form) {
+    let formData = new FormData(form);
+    let response = await fetch('request.php', {
+        method: 'POST',
+        body: formData
+    });
+    let result = await response.text();
+    console.log(result);
+    if (result == 'Student registered successfully') {
+        alert('Student registered successfully');
+    } else if (result == "Error while registering student") {
+        alert('Error while registering student');
+    }
+
+}
+
+
+getGrade();
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    myRegisterStudent(form);
+});
